e5cc88abec576ec8c77cd863734fc2c351b559d4
sam-codez1 <sfoshee2@gmail.com>
2023-01-03 18:58:09 -0700

    updated the text share image+message

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Spellbook" key="title"/>
+        <meta property="og:description" content="summon your next campaign" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://lexica-serve-encoded-images2.sharif.workers.dev/md/60ed3ca0-8fd7-4943-be40-fd6277aaeae3"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
