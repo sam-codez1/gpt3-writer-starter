4bd10a9211817450c4bb7449be951597fa7b33a7
sam-codez1 <sfoshee2@gmail.com>
2023-01-08 17:01:09 -0600

    continued prompt updates

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -25,7 +25,7 @@ const generateAction = async (req, res) => {
 
   const secondPrompt =
   `
-  Take the Dungeons and Dragons Campaign below and generate 5 quest lines. Make them feel like an RPG. Go into detail with each one, including a Boss fight and the rewards for the party.
+  Take the Dungeons and Dragons Campaign below and generate 5 quest lines. Make them feel like an RPG. Go deep into detail with each one, including a Boss fight and the rewards for the party. Make sure to mention the names of the main characters a few times.
   Make sure every quest leads into the next quest to create a single overarching story.
   
   Dungeons and Dragons Campaign: ${basePromptOutput.text}
